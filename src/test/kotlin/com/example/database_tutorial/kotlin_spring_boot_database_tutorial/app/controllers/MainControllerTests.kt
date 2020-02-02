417dde3eccb2c3c1c286da06b9fc885c26ad31fb
cln-m4rie <s.kikkawa0613@gmail.com>
2020-02-02 22:57:29 +0900

    add update testing

--- src/test/kotlin/com/example/database_tutorial/kotlin_spring_boot_database_tutorial/app/controllers/MainControllerTests.kt
+++ src/test/kotlin/com/example/database_tutorial/kotlin_spring_boot_database_tutorial/app/controllers/MainControllerTests.kt
@@ -1,11 +1,13 @@
 package com.example.database_tutorial.kotlin_spring_boot_database_tutorial.app.controllers
 
+import com.example.database_tutorial.kotlin_spring_boot_database_tutorial.domain.entity.User
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -44,6 +46,20 @@ class MainControllerTests {
                 .andExpect(status().isOk)
                 .andExpect(
                         content().string("Saved")
+                )
+    }
+
+    @Test
+    @Sql(statements = ["insert into user(name) values ('update_data');"])
+    fun updateUserTest() {
+        val lastUser: User = target.userRepository.findAll().last()
+        mockMvc.perform(
+                post("/update")
+                        .param("id", lastUser.id.toString())
+                        .param("name", "updated!!!"))
+                .andExpect(status().isOk)
+                .andExpect(
+                        content().string("Updated")
                 )
     }
 }
